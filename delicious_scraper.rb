5506a9286c33c90bad71b648246f436cfa3fe954
Taylor Brooks <tbrooks@gmail.com>
2014-03-14 10:31:20 -0500

    Added gitignore and support for getting a tag list

--- delicious_scraper.rb
+++ delicious_scraper.rb
@@ -3,6 +3,7 @@ require 'csv'
 require 'json'
 require 'net/http'
 
+
 def scrape(username, tag)
   url = "http://www.delicious.com/v2/json/#{username}/#{tag}?count=1000"
   resp = Net::HTTP.get_response(URI.parse(url))
@@ -20,6 +21,23 @@ def scrape(username, tag)
     csv << ["URL", "Notes", "Tags", "Description"]
     result.each do |r|
       csv << ["#{r[:u]}", "#{r[:n]}", "#{r[:t]}", "#{r[:d]}"]
+    end
+  end
+end
+
+def get_tags(username)
+  url = "http://feeds.delicious.com/v2/json/tags/#{username}?count=1000"
+  resp = Net::HTTP.get_response(URI.parse(url))
+  data = resp.body
+
+  # we convert the returned JSON data to native Ruby
+  # data structure - a hash
+  result = JSON.parse(data, :symbolize_names => true)
+
+  CSV.open("#{username}-delicious-tags.csv", "wb") do |csv|
+    csv << ["tag", "count"]
+    result.each do |r|
+      csv << [r[0].to_s, r[1].to_s]
     end
   end
 end
